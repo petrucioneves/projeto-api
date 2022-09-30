bdb0622df0a3066ed47f171614c2a576a9efdba1
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-09-30 13:39:21 -0300

    Gerenciador de conexao com o DataBase

---
+++ src/config/db.ts
@@ -0,0 +1,11 @@
+import  {createConnection} from 'typeorm'
+
+export const conectarSevidorNoBD = async() =>{
+    const conexao = await createConnection();
+    console.log('App conectado ao database ${conexao.options.database}');
+    process.on('SIGINT', () => {
+        conexao.close().then(() => {
+            console.log('Conexao com o DB fechada');
+        });
+    });
+};
