f546a5dca87cb2cb75dbef0b08fe35f73374581d
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-09-30 14:09:17 -0300

    Gerenciador de conexao com o DataBase

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,8 @@
 import  {createConnection} from 'typeorm'
 
-export const conectarSevidorNoBD = async() =>{
+export const conectarSevidorNoBD = async () =>{
     const conexao = await createConnection();
-    console.log('App conectado ao database ${conexao.options.database}');
+    console.log(`App conectado ao database ${conexao.options.database}`);
     process.on('SIGINT', () => {
         conexao.close().then(() => {
             console.log('Conexao com o DB fechada');
